afb3a8354e4177c682d0c3a0e8eba056447a141f
Mauricio <mauparra80@hotmail.com>
2024-03-04 13:14:34 -0600

    backup save

--- src/modules/footer/footer.jsx
+++ src/modules/footer/footer.jsx
@@ -1,9 +1,90 @@
-
+import { Link } from 'react-router-dom';
+import "./footerStyle.css";
+import GithubMark from '../../assets/github-mark.png'
 
 export default function Footer() {
   return (
     <div id="footer">
-      <h1>footer</h1>
+      <div className="footer-section footer-main">
+        <a href="https://github.com/mauparra80" className='signature'>
+          <img src={GithubMark} alt="github mark" />
+          <p>Mauparra80</p>
+        </a>
+        <div className="links">
+          <h3>Links</h3>
+          <ul>
+            <li>
+              <Link>Responsability</Link>
+            </li>
+            <li>
+              <Link>Manufacturing</Link>
+            </li>
+            <li>
+              <Link>Affiliate Dashboard</Link>
+            </li>
+            <li>
+              <Link>Contact Us</Link>
+            </li>
+            <li>
+              <Link>About Us</Link>
+            </li>
+            <li>
+              <Link>Find Us</Link>
+            </li>
+            <li>
+              <Link>Blog</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="customer-care">
+          <h3>Customer Care</h3>
+          <ul>
+            <li>
+              <Link>FAQs</Link>
+            </li>
+            <li>
+              <Link>Shipping</Link>
+            </li>
+            <li>
+              <Link>Returns & Exchanges</Link>
+            </li>
+            <li>
+              <Link>Product Care & Instructions</Link>
+            </li>
+            <li>
+              <Link>Ethical Gurarantee</Link>
+            </li>
+            <li>
+              <Link>Repairs</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr className='footer-line' />
+      <div className="footer-section footer-bottom">
+        <ul>
+          <li>
+            <p>&copy;2024 <Link>Company</Link></p>
+          </li>
+          <span>&middot;</span>
+          <li>
+            <Link>Refund policy</Link>
+          </li>
+          <span>&middot;</span>
+          <li>
+            <Link>Privacy policy</Link>
+          </li>
+          <span>&middot;</span>
+          <li>
+            <Link>Terms of services</Link>
+          </li>
+          <span>&middot;</span>
+          <li>
+            <Link>Contact information</Link>
+          </li>
+        </ul>
+      </div>
+
       <p>UIcons by <a href="https://www.flaticon.com/uicons">Flaticon</a></p>
     </div>
   )
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -24,7 +24,7 @@ export default function Home() {
   
   return (
     <div id="home" className="body-container">
-      <div className="splash-container">
+      <div className="splash-container section-container">
         <div className="splash-image"><img src={splash1} alt="fashion model" /></div>
         <div className="splash-image"><img src={splash2} alt="fashion model" /></div>
         <div className="splash-image"><img src={splash3} alt="fashion model" /></div>
@@ -33,6 +33,15 @@ export default function Home() {
         <h3>Mystic Mode</h3>
         <p>Fashion that transends time</p>
         <button className="main-button">Shop Now</button>
+      </div>
+      <div className="section-container warranty-prompt">
+        <div className="diamond-container">
+          <div className="diamond"></div>
+          <h3>QUALITY MADE</h3>
+        </div>
+        <h2>OUR LIFETIME GURANTEE</h2>
+        <p>We mean it when we say our clothing and gear are quality made. Thats why we offer a lifetime guarantee with all of our products.</p>
+        <button className="main-button">Find out more</button>
       </div>
     </div>
   )
